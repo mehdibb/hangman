c7a7cdc37b6bc09fc492f99e786a440ce201f0da
Mehdi Babapour <mehdi@pragmateam.dev>
2023-11-10 03:27:26 +0330

    feat(initial): hardcoded impl

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [{hostname: 'as2.ftcdn.net'}],
+  }
+}
 
 module.exports = nextConfig
