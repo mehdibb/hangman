3b2eeea8d96056533fe55166f6cbc5b1f72fdf23
Mehdi Babapour <mehdi@pragmateam.dev>
2023-11-17 22:48:41 +0330

    feat(matches): save the matches for logged in users

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,17 +8,31 @@ import { authOptions } from '@/lib/auth';
 export async function Main(): Promise<ReactElement> {
   const session = await getServerSession(authOptions);
 
-  await db.connect();
+  const chosenWord = (
+    (await db.query('SELECT * FROM words ORDER BY RANDOM() LIMIT 1'))
+      .rows as Array<{ id: number; word: string }>
+  )[0];
 
-  const result = (
-    await db.query('SELECT * FROM words ORDER BY RANDOM() LIMIT 1')
-  ).rows as Array<{ id: number; word: string }>;
+  if (session != null) {
+    const user = (
+      (
+        await db.query('SELECT * FROM users WHERE email = $1', [
+          session.user?.email,
+        ])
+      ).rows as Array<{ email: string; id: string }>
+    )[0];
+
+    await db.query('INSERT INTO matches (user_id, word_id) VALUES ($1, $2)', [
+      user.id,
+      chosenWord.id,
+    ]);
+  }
 
   return (
     <>
       <h1 className="text-xl font-bold sm:mb-8 sm:text-4xl">Hangman</h1>
       <Navbar username={session?.user?.email ?? undefined} />
-      <Game word={result[0].word.toUpperCase()} />
+      <Game word={chosenWord.word.toUpperCase()} />
     </>
   );
 }
