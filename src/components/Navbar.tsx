03ed9d316c47c796bcc7ce3470e8f601563eab8b
Mehdi Babapour <mehdi@pragmateam.dev>
2023-11-20 01:41:05 +0330

    feat: add leaderboard to navbar dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,20 +6,10 @@ import Link from 'next/link';
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { useSession } from 'next-auth/react';
-import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
-
-const navigation: Array<{ href: string; name: string }> = [
-  {
-    href: '/leaderboard',
-    name: 'Leaderboard',
-  },
-];
 
 export function Navbar(): ReactElement {
   const session = useSession();
-
-  const pathname = usePathname();
 
   const username = session?.data?.user?.email;
   return (
@@ -33,25 +23,6 @@ export function Navbar(): ReactElement {
             <Link href="/" className="flex shrink-0 items-center text-lg">
               Hangman
             </Link>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className={cn(
-                      item.href === pathname
-                        ? 'bg-gray-900 text-white'
-                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'rounded-md px-3 py-2 text-sm font-medium'
-                    )}
-                    aria-current={item.href === pathname ? 'page' : undefined}
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <Menu as="div" className="relative ml-3">
@@ -73,60 +44,56 @@ export function Navbar(): ReactElement {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
               >
-                <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/matches"
-                        className={cn(
-                          active ? 'bg-gray-100' : '',
-                          'block px-4 py-2 text-sm text-gray-700'
-                        )}
-                      >
-                        Matches
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/api/auth/signout"
-                        className={cn(
-                          active ? 'bg-gray-100' : '',
-                          'block px-4 py-2 text-sm text-gray-700'
-                        )}
-                      >
-                        Sign out
-                      </Link>
-                    )}
-                  </Menu.Item>
+                <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right divide-y divide-gray-200 rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none">
+                  <div>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href="/matches"
+                          className={cn(
+                            active ? 'bg-gray-100' : '',
+                            'block px-4 py-2 text-sm text-gray-700'
+                          )}
+                        >
+                          Matches
+                        </Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href="/leaderboard"
+                          className={cn(
+                            active ? 'bg-gray-100' : '',
+                            'block px-4 py-2 text-sm text-gray-700'
+                          )}
+                        >
+                          Leaderboard
+                        </Link>
+                      )}
+                    </Menu.Item>
+                  </div>
+                  <div>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href="/api/auth/signout"
+                          className={cn(
+                            active ? 'bg-gray-100' : '',
+                            'block px-4 py-2 text-sm text-gray-700'
+                          )}
+                        >
+                          Sign out
+                        </Link>
+                      )}
+                    </Menu.Item>
+                  </div>
                 </Menu.Items>
               </Transition>
             </Menu>
           </div>
         </div>
       </div>
-
-      <Disclosure.Panel className="sm:hidden">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          {navigation.map((item) => (
-            <Disclosure.Button
-              key={item.name}
-              as="a"
-              href={item.href}
-              className={cn(
-                item.href === pathname
-                  ? 'bg-gray-900 text-white'
-                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                'block rounded-md px-3 py-2 text-base font-medium'
-              )}
-              aria-current={item.href === pathname ? 'page' : undefined}
-            >
-              {item.name}
-            </Disclosure.Button>
-          ))}
-        </div>
-      </Disclosure.Panel>
     </Disclosure>
   );
 }
