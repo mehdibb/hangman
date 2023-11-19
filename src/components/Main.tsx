e35327a8cbf2478c2405b9fde2a4b45abb05cdeb
Mehdi Babapour <mehdi@pragmateam.dev>
2023-11-19 04:00:21 +0330

    feat(matches): only keep the latest in_progress match

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -12,6 +12,8 @@ export async function Main(): Promise<ReactElement> {
       .rows as Array<{ id: number; word: string }>
   )[0];
 
+  let match: { id: number } | undefined;
+
   if (session != null) {
     const user = (
       (
@@ -21,11 +23,25 @@ export async function Main(): Promise<ReactElement> {
       ).rows as Array<{ email: string; id: string }>
     )[0];
 
-    await db.query('INSERT INTO matches (user_id, word_id) VALUES ($1, $2)', [
-      user.id,
-      chosenWord.id,
-    ]);
+    await db.query(
+      `DELETE FROM matches 
+          WHERE id = (
+              SELECT id 
+              FROM matches
+              WHERE user_id = $1 AND result = 'in_progress' 
+              ORDER BY match_timestamp DESC 
+              LIMIT 1
+        );`,
+      [user.id]
+    );
+
+    match = (
+      await db.query(
+        'INSERT INTO matches (user_id, word_id) VALUES ($1, $2) RETURNING id;',
+        [user.id, chosenWord.id]
+      )
+    ).rows[0] as { id: number };
   }
 
-  return <Game word={chosenWord.word.toUpperCase()} />;
+  return <Game word={chosenWord.word.toUpperCase()} matchId={match?.id} />;
 }
